ce5aa0775f6da57b90aef950591eb3e132ab6533
ValenzuelaRa <al20760222@ite.edu.mx>
2023-10-11 19:47:21 -0700

    lexico

--- LexicoSQL/lexico.js
+++ LexicoSQL/lexico.js
@@ -1,9 +1,29 @@
 const fs = require('fs');
-
 fs.readFile('./sp.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error al leer el archivo:', err);
     return;
   }
-  console.log(data);
+  const tokens = '.,;!@=()*_:"="\r\n'
+  let palabra = ''
+  const resultado = [];
+
+data.split('').forEach((caracter) => {
+    if (tokens.includes(caracter)) {
+        if (palabra !== "") {
+            resultado.push(palabra);
+            palabra = "";
+        }
+        resultado.push(caracter);
+    } else {
+        palabra += caracter;
+    }
+});
+
+if (palabra !== "") {
+    resultado.push(palabra);
+}
+
+console.log(resultado);
+
 });
