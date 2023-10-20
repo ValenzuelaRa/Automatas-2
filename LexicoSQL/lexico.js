3e88cf8bbcdba36581bdc4ae9d55267e035ec148
ValenzuelaRa <al20760222@ite.edu.mx>
2023-10-20 13:57:49 -0700

    Asginacion de variables, palabraReservadas Es un array que se utiliza para almacenar las palabras reservadas, Tokens, un array para almacenar los numeros de cada palabra o caracter, ademas de dos variables temporales

--- LexicoSQL/lexico.js
+++ LexicoSQL/lexico.js
@@ -25,30 +25,26 @@ fs.readFile('./sp.txt', 'utf8', (err, data) => {
         }
         resultado.push(caracter);
     } else {
-        // Si el carácter no es un token, agrégalo a la palabra actual
         palabra += caracter;
     }
   }
-  // Imprime el resultado final después de la tokenización
   console.log(resultado);
 
-  // Bloque de lectura y procesamiento del archivo 'sqlkeywords.txt'
+  
   fs.readFile('./sqlkeywords.txt', 'utf8', (err, data2) => {
-    // Manejo de errores en la lectura del archivo
     if (err) {
       console.error('Error al leer el archivo:', err);
       return;
     }
 
-    // Inicializa variables para procesamiento
-    let palabra = ''; // Nota: Esta variable está siendo redefinida, podría causar problemas
-    let tokens = []; // Nota: Esta variable está vacía y no se utiliza en este contexto
-    let palabrasReservadas = []; // Nota: Esta variable está siendo definida pero no se utiliza
-    let resultado2 = ''; // Nota: Esta variable está siendo definida pero no se utiliza
+        let palabrasReservadas = [];
+        let tokens = [];
+        let banderaPalabraReservada = '';
+        let banderaTokens = '';
 
     // Itera sobre cada carácter en el archivo (nota: debería ser 'data2' en lugar de 'data')
-    for (let i = 0; i < data.length; i++) {
-      let caracter = data[i];
+    for (let i = 0; i < data2.length; i++) {
+      let caracter = data2[i];
       // Lógica de tokenización y procesamiento (incompleta y con problemas)
       if (tokens.includes(caracter)) {
           if (palabra !== "") {
