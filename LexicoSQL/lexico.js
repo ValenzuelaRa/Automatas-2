6e49dc451f4aec14b6ffcfaa073b05159176fc48
ValenzuelaRa <al20760222@ite.edu.mx>
2023-10-19 20:21:48 -0700

    Agregue el sqlkeywords.txt

--- LexicoSQL/lexico.js
+++ LexicoSQL/lexico.js
@@ -1,29 +1,54 @@
 const fs = require('fs');
+
+
 fs.readFile('./sp.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error al leer el archivo:', err);
     return;
   }
-  const tokens = '.,;!@=()*_:"="\r\n '
+  
+  const tokens = '\'\n\r*@:,;=.() '
   let palabra = ''
-  const resultado = [];
+  let resultado = []
 
-data.split('').forEach((caracter) => {
+  for (let i = 0; i < data.length; i++) {
+    let caracter = data[i];
     if (tokens.includes(caracter)) {
-        if (palabra !== " ") {
+        if (palabra !== "") {
             resultado.push(palabra);
-            palabra = " ";
+            palabra = "";
         }
         resultado.push(caracter);
     } else {
         palabra += caracter;
     }
-});
+  }
+  console.log(resultado);
+  
 
-if (palabra !== " ") {
-    resultado.push(palabra);
-}
 
-console.log(resultado);
+fs.readFile('./sqlkeywords.txt', 'utf8', (err, data2) => {
+  if (err) {
+    console.error('Error al leer el archivo:', err);
+    return;
+  }
 
+  let palabra = ''
+  let tokens = []
+  let palabrasReservadas = []
+  let resultado2 = ''
+
+  for (let i = 0; i < data.length; i++) {
+    let caracter = data[i];
+    if (tokens.includes(caracter)) {
+        if (palabra !== "") {
+            resultado.push(palabra);
+            palabra = "";
+        }
+        resultado.push(caracter);
+    } else {
+        palabra += caracter;
+    }
+  }
+  })
 });
