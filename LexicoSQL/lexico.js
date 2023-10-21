cedde8713e4e56ba8ccb84f30fd983b04b0cd102
ValenzuelaRa <106852852+ValenzuelaRa@users.noreply.github.com>
2023-10-20 19:25:22 -0700

    Actualizar lexico.js

--- LexicoSQL/lexico.js
+++ LexicoSQL/lexico.js
@@ -1,61 +1,73 @@
-
 const fs = require('fs');
 
-fs.readFile('./sp.txt', 'utf8', (err, data) => {
-
-  if (err) {
-    console.error('Error al leer el archivo:', err);
-    return;
-  }
-
-
-  const caracteres = [';', ':', "'", '=', '(', ')', '"', '*', '.', ' ', '@', '[', ']', '|', '\n', '\r'];
-  
-  let palabra = '';
-  let resultado = [];
-
-
-  for (let i = 0; i < data.length; i++) {
-    let caracter = data[i];
-
-    if (tokens.includes(caracter)) {
-        if (palabra !== "") {
-            resultado.push(palabra);
-            palabra = "";
-        }
-        resultado.push(caracter);
-    } else {
-        palabra += caracter;
-    }
-  }
-  console.log(resultado);
-
-  
-  fs.readFile('./sqlkeywords.txt', 'utf8', (err, data2) => {
+// Lee el archivo 'procedure.txt'
+fs.readFile('sp.txt', 'utf-8', (err, data) => {
     if (err) {
-      console.error('Error al leer el archivo:', err);
-      return;
+        console.error(err);
+        return;
     }
 
+    // Define los caracteres especiales
+    const caracteresEspeciales = [';', ':', "'", '=', '(', ')', '"', '*', '.', ' ', '@', '[', ']', '|', '\n', '\r'];
+
+    // Inicializa variables para tokenización
+    let resultado = [];
+    let palabra = '';
+
+    // Itera sobre cada carácter en el archivo
+    for (let i = 0; i < data.length; i++) {
+        const caracter = data[i];
+        if (caracteresEspeciales.includes(caracter)) {
+            if (palabra !== '') {
+                resultado.push(palabra.toUpperCase()); // Normaliza a mayúsculas
+                palabra = '';
+            }
+            resultado.push(caracter);
+        } else {
+            palabra += caracter;
+        }
+    }
+
+    // Lee el archivo 'sqlkeywords.txt' para palabras reservadas y números
+    fs.readFile('sqlkeywords.txt', 'utf-8', (err, datos) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        // Define los caracteres separadores
+        const caracteresSeparadores = [' ', '"'];
+
+        // Inicializa variables para palabras reservadas y números
         let palabrasReservadas = [];
         let tokens = [];
-        let banderaPalabraReservada = '';
-        let banderaTokens = '';
+        let temporalPalabraReservada = '';
+        let temporalTokens = '';
 
-    // Itera sobre cada carácter en el archivo (nota: debería ser 'data2' en lugar de 'data')
-    for (let i = 0; i < data2.length; i++) {
-      let caracter = data2[i];
-      // Lógica de tokenización y procesamiento (incompleta y con problemas)
-      if (tokens.includes(caracter)) {
-          if (palabra !== "") {
-              resultado.push(palabra);
-              palabra = "";
-          }
-          resultado.push(caracter);
-      } else {
-          palabra += caracter;
-      }
-    }
-    // Nota: No se está haciendo nada con 'resultado2' después de este procesamiento
-  });
+        // Itera sobre cada línea en el archivo 'sqlkeywords.txt'
+        const lineas = datos.split('\n');
+        lineas.forEach(linea => {
+            const partes = linea.trim().split(' : ');
+            if (partes.length === 2) {
+                const palabraReservada = partes[1].replace(/"/g, '').trim().toUpperCase(); // Quita comillas y normaliza a mayúsculas
+                palabrasReservadas.push(palabraReservada);
+                tokens.push(partes[0]);
+            }
+        });
+
+        for (let i = 0; i < resultado.length; i++) {
+            let tokenAsignado = '';
+            for (let j = 0; j < palabrasReservadas.length; j++) {
+                if (palabrasReservadas[j] === resultado[i].toUpperCase()) {
+                    console.log(`Clave: ${resultado[i]} Token: ${tokens[j]}`);
+                    tokenAsignado = tokens[j];
+                    break;
+                }
+            }
+            // Si la palabra no está en la lista de palabras reservadas, imprímela también
+            if (tokenAsignado === '' && resultado[i] !== '\n' && resultado[i] !== '\r') {
+                console.log(`Palabra: ${resultado[i]} (No tiene token asignado)`);
+            }
+        }
+    });
 });
