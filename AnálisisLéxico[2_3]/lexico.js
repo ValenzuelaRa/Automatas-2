1715e9e7b3309d455383d98a34e81e92d3df9b13
ValenzuelaRa <al20760222@ite.edu.mx>
2023-11-10 17:22:58 -0800

    lexicos

--- AnálisisLéxico[2_3]/lexico.js
+++ AnálisisLéxico[2_3]/lexico.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
-fs.readFile(SQL, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error al leer el archivo:', err);
-    return;
-  }
+// Lee el archivo 'procedure.txt'
+fs.readFile('sp.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
 
     //Se inicializa un array con los signos y espacios que contiene el documento
     const signos = [".",",",";","!","@","=","(",")","*","_",",",":","="," ", "\r","\n","'"] 
@@ -13,27 +14,28 @@ fs.readFile(SQL, 'utf8', (err, data) => {
     let palabra = ''
     //Se inicializa un array vacio que contendra el resultado final
     const resultado = [];
-  
+    //Se recorre cada caracter del documento, en total son 194 caracterest
     for (let i = 0; i < data.length; i++) {
-  
+       // data[i] vale 0, donde el valor es "S" la cual se guarda como el primer caracter
       const caracter = data[i];
-  
+     //"S" se incluye en el arreglo de signos? False
       if (signos.includes(caracter)) {
+        
           if (palabra !== "") {
               resultado.push(palabra);
               palabra = "";
           }
+          
           resultado.push(caracter);
+          //se ejecuta el bloque else
       } else {
+        //palabra se actualiza y deja de ser una cadena vacia, ahora vale "S"
           palabra += caracter;
       }
   }
+
   
-  if (palabra !== "") {
-      resultado.push(palabra);
-  }
-  
-  console.log(resultado);
+
   })
 
 
